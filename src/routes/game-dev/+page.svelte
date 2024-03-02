dfc8a238295e85e679bdbe7cc2adce7c894e8b47
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-03-01 20:13:09 -0600

    remove problem set list, change loc for project list

--- src/routes/game-dev/+page.svelte
+++ src/routes/game-dev/+page.svelte
@@ -1,39 +1,15 @@
 <script>
 	import SvelteMarkdown from 'svelte-markdown';
 
-	const tenativeSchedule = `
-| Date | Topic |
-| :--- | :--- |
-| Week 0 | Introductions |
-| Week 1 | Scratch Week |
-| Week 2 | Introducing Python |
-| Weeks 3 - 4 | Hangman |
-| Weeks 5 - 6 | Pong |
-| Weeks 7 - 8| Snake Game |
-| Weeks 9 - 10 | Tetris |
-| Weeks 11 - 13 | Space Invaders |  |
-| Weeks 14 - 16 | Side-scroller / Platform |
-| Weeks 17 - 19 | Final Projects |
-| Week 20 | Goodbyes / End-of-semester celebration |`;
-
 	export let data;
 </script>
 
 <div class="container">
 	<h1>Python Game Dev Course</h1>
 	<a href="game-dev/syllabus">Syllabus</a>
-	<div>
-		<h2>Problem Sets</h2>
-	</div>
-	<a href="https://github.com/northridge-dev/python-game-dev/tree/main/problem_sets"
-		>all problem sets</a
-	>
-	<div class="box">
-		<SvelteMarkdown source={data.problemSets} />
-	</div>
 
 	<div>
-		<h2>Projects</h2>
+		<h2>Project Library</h2>
 	</div>
 	<a href="https://github.com/northridge-dev/python-game-dev/tree/main/projects">all projects</a>
 	<div class="box">
--- src/routes/game-dev/+page.ts
+++ src/routes/game-dev/+page.ts
@@ -1,9 +1,8 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-	const [problemSets, projects, classLogRaw] = await Promise.all(
+	const [projects, classLogRaw] = await Promise.all(
 		[
-			'https://raw.githubusercontent.com/northridge-dev/python-game-dev/main/problem_sets/list.md',
-			'https://raw.githubusercontent.com/northridge-dev/python-game-dev/main/projects/list.md',
+			'https://raw.githubusercontent.com/northridge-dev/python-game-dev/main/projects/README.md',
 			'https://raw.githubusercontent.com/northridge-dev/python-game-dev/main/log/2024_01.md'
 		].map((url) => fetch(url).then((response) => response.text()))
 	);
@@ -11,7 +10,6 @@ export async function load({ fetch }) {
 	const classLog = classLogRaw.slice(classLogRaw.indexOf('|')).trim();
 
 	return {
-		problemSets,
 		projects,
 		classLog
 	};
